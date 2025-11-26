591dafb1aab70ba4098df2cef48850e8b92d38d3
arayjesua <alu0101717623@ull.edu.es>
2025-11-26 18:36:13 +0000

    compressor

--- Unidad_3/backup-server.cc
+++ Unidad_3/backup-server.cc
@@ -12,6 +12,8 @@
 #include <cstdlib>
 #include <expected>
 #include <atomic>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 const int buffer_size = 65536;
 std::atomic<bool> quit_requested{false};
@@ -31,6 +33,19 @@ enum class ParseArgsErrors {
   unknown_option,
   multiple_compression_options,
   too_many_arguments
+};
+
+enum class CopyFileCompressedError {
+  error_creando_pipe,
+  error_escribiendo_en_pipe,
+  error_leyendo_de_pipe,
+  error_creando_hijo,
+  error_abriendo_archivo_destino,
+  error_ejecutando_exec,
+  error_bloqueando_señal,
+  error_abriendo_archivo_origen,
+  error_leyendo_archivo_origen,
+  error_hijo_terminado_por_señal
 };
 
 std::string get_environment_variable(const std::string& name) {
@@ -304,7 +319,153 @@ std::expected<void, std::system_error> copy_file(const std::string& src_path, co
   return {};
 }
 
-void run_server(int fifo_fd, const std::string& backup_dir) {
+std::string errores(CopyFileCompressedError& error) {
+  switch (error) {
+    case CopyFileCompressedError::error_abriendo_archivo_destino:
+      return "backup-server: error: error abriendo el archivo de destino\n";
+
+    case CopyFileCompressedError::error_abriendo_archivo_origen:
+      return "backup-server: error: error abriendo el archivo de origen\n";
+
+    case CopyFileCompressedError::error_bloqueando_señal:
+      return "backup-server: error: error bloqueando la señal SIGPIPE\n";
+
+    case CopyFileCompressedError::error_creando_hijo:
+      return "backup-server: error: error creando el proceso hijo\n";
+
+    case CopyFileCompressedError::error_creando_pipe:
+      return "backup-server: error: error creando el pipe\n";
+
+    case CopyFileCompressedError::error_ejecutando_exec:
+      return "backup-server: error: error ejecutando execvp\n";
+
+    case CopyFileCompressedError::error_escribiendo_en_pipe:
+      return "backup-server: error: error escribiendo en el pipe\n";
+
+    case CopyFileCompressedError::error_hijo_terminado_por_señal:
+      return std::string();
+
+    case CopyFileCompressedError::error_leyendo_archivo_origen:
+      return "backup-server: error: error leyendo el archivo de origen\n";
+
+    case CopyFileCompressedError::error_leyendo_de_pipe:
+      return "backup-server: error: error leyendo del pipe\n";
+
+    default:
+      break;
+  }
+  return std::string();
+}
+
+std::string get_compression_extension(ServerOptions::CompressionType compression) {
+  switch (compression) {
+    case ServerOptions::CompressionType::BZIP2:
+      return ".bz2";
+    
+    case ServerOptions::CompressionType::GZIP:
+      return ".gz";
+
+    case ServerOptions::CompressionType::XZ:
+      return ".xz";
+    
+    default:
+      break;
+  }
+  return std::string();
+}
+
+std::expected<void, CopyFileCompressedError> copy_file_compressed(const std::string& src_path, const std::string& dest_path, const std::string& compression_command) {
+  int pipefd[2];
+  int fallo = pipe(pipefd);
+  if (fallo == -1) {
+    return std::unexpected(CopyFileCompressedError::error_creando_pipe);
+  }
+  pid_t hijo = fork();
+  if (hijo == -1) {
+    return std::unexpected(CopyFileCompressedError::error_creando_hijo);
+  } else if (hijo == 0) {
+      dup2(pipefd[0], 0);
+      close(pipefd[0]);
+      close(pipefd[1]);
+      int abierto = open(dest_path.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      if (abierto == -1) {
+        return std::unexpected(CopyFileCompressedError::error_abriendo_archivo_destino);
+      }
+      dup2(abierto, 1);
+      close(abierto);
+      char* args[] = { const_cast<char*>(compression_command.c_str()), nullptr };
+      execvp(compression_command.c_str(), args);
+      return std::unexpected(CopyFileCompressedError::error_ejecutando_exec);
+      std::exit(EXIT_SUCCESS);
+  } else {
+      int señal = sigemptyset(&conjunto_señales);
+      if (señal == -1) {
+        return std::unexpected(CopyFileCompressedError::error_bloqueando_señal);
+      }
+      señal = sigaddset(&conjunto_señales, SIGUSR1);
+      if (señal == -1) {
+        return std::unexpected(CopyFileCompressedError::error_bloqueando_señal);
+      }
+      señal = sigprocmask(SIG_BLOCK, &conjunto_señales, NULL);
+      if (señal == -1) {
+        return std::unexpected(CopyFileCompressedError::error_bloqueando_señal);
+      }
+      close(pipefd[0]);
+      int tamaño;
+      std::string datos(buffer_size, '\0');
+      int abierto = open(src_path.c_str(), O_RDONLY);
+      if (abierto == -1) {
+        return std::unexpected(CopyFileCompressedError::error_abriendo_archivo_origen);
+      }
+      while ((tamaño = read(abierto, datos.data(), buffer_size)) > 0) {
+        datos.resize(tamaño);
+        write(pipefd[1], datos.data(), tamaño);
+      }
+      if (tamaño == -1) {
+        return std::unexpected(CopyFileCompressedError::error_leyendo_archivo_origen);
+      }
+      close(pipefd[1]);
+      int status;
+      waitpid(hijo, &status, 0);
+      if (WIFEXITED(status)) {
+        int exit_code = WEXITSTATUS(status);
+        std::string correcto = "Hijo terminó con código ";
+        correcto += std::to_string(exit_code) + '\n';
+        write(STDOUT_FILENO, correcto.c_str(), correcto.length());
+      } else if (WIFSIGNALED(status)) {
+          int signal = WTERMSIG(status);
+          std::string terminado = "Hijo terminado por señal ";
+          terminado += std::to_string(signal);
+          write(STDOUT_FILENO, terminado.c_str(), terminado.length());
+          return std::unexpected(CopyFileCompressedError::error_hijo_terminado_por_señal);
+      }
+  }
+  return {};
+}
+
+std::string get_compression_command(ServerOptions::CompressionType compression) {
+  std::string compression_type;
+  switch (compression) {
+    case ServerOptions::CompressionType::BZIP2:
+      compression_type = "bzip2";
+      break;
+    
+    case ServerOptions::CompressionType::GZIP:
+      compression_type = "gzip";
+      break;
+
+    case ServerOptions::CompressionType::XZ:
+      compression_type = "xz";
+      break;
+
+    default:
+      compression_type = "none";
+      break;
+  }
+  return compression_type;
+}
+
+void run_server(int fifo_fd, const std::string& backup_dir, ServerOptions& opciones) {
   siginfo_t sig;
   while (!quit_requested) {
     int espera = sigwaitinfo(&conjunto_señales, &sig);
@@ -337,16 +498,33 @@ void run_server(int fifo_fd, const std::string& backup_dir) {
       }
       std::string archivo = get_filename(path);
       std::string destino_backup = backup_dir + "/" + archivo;
-      auto copia = copy_file(path, destino_backup, 0666);
-      if (copia.has_value()) {
-        const char* exito = "ARCHIVO COPIADO CON ÉXITO\n";
-        write (STDOUT_FILENO, exito, std::strlen(exito));
+      if (opciones.compression == ServerOptions::CompressionType::NONE) {
+        auto copia = copy_file(path, destino_backup, 0666);
+        if (copia.has_value()) {
+          const char* exito = "ARCHIVO COPIADO CON ÉXITO\n";
+          write (STDOUT_FILENO, exito, std::strlen(exito));
+        } else {
+            std::system_error error = copia.error();
+            std::string msg_error{error.what()};
+            msg_error += "\n";
+            write(STDERR_FILENO, msg_error.c_str(), msg_error.length());
+            continue;
+        }
       } else {
-          std::system_error error = copia.error();
-          std::string msg_error{error.what()};
-          msg_error += "\n";
-          write(STDERR_FILENO, msg_error.c_str(), msg_error.length());
-          continue;
+          std::string extension = get_compression_extension(opciones.compression);
+          destino_backup += extension;
+          std::string comando = get_compression_command(opciones.compression);
+          auto copia = copy_file_compressed(path, destino_backup, comando);
+          if (copia.has_value()) {
+            std::string exito = "backup completado: ";
+            exito += path + " -> " + destino_backup + '\n';
+            write (STDOUT_FILENO, exito.c_str(), exito.length());
+          } else {
+            CopyFileCompressedError error = copia.error();
+            std::string msg_error{errores(error)};
+            write(STDERR_FILENO, msg_error.c_str(), msg_error.length());
+            continue;
+          }
       }
     }
   }
@@ -372,28 +550,39 @@ void run_server(int fifo_fd, const std::string& backup_dir) {
 std::expected<ServerOptions, ParseArgsErrors> parse_arguments(int argc, char* argv[]) {
   ServerOptions opciones;
   bool flag{false};
-  std::vector<std::string_view> args(argv + 1, argv + argc);
-  for (auto it = args.begin(), end = args.end(); it != end; ++it) {
-    if (argc == 2 && it->starts_with("-")) {
-      opciones.destino_backup = get_current_dir_name();
-    }
-    if (*it == "-z" && !flag) {
-      opciones.compression = ServerOptions::CompressionType::GZIP; 
-      flag = true;
-    } else if (*it == "-j" && !flag) {
-        opciones.compression = ServerOptions::CompressionType::BZIP2;
-        flag = true;
-    } else if (*it == "-x" && !flag) {
-        opciones.compression =  ServerOptions::CompressionType::XZ;
-        flag = true;
-    } else if (!it->starts_with("-")) {
-        opciones.destino_backup = *it;
-    } else if ((*it == "-z" || *it == "-j" || *it == "-x") && flag) {
+  for (int opt; (opt = getopt(argc, argv, "zjx")) != -1;) {
+    if (!flag) {
+      switch (opt) {
+        case 'z':
+          opciones.compression = ServerOptions::CompressionType::GZIP; 
+          flag = true;
+          break;
+        
+        case 'j':
+          opciones.compression = ServerOptions::CompressionType::BZIP2;
+          flag = true;
+          break;
+        
+        case 'x':
+          opciones.compression =  ServerOptions::CompressionType::XZ;
+          flag = true;
+          break;
+        
+        default:
+          return std::unexpected(ParseArgsErrors::unknown_option);
+      }
+    } else if ((opt == 'z' || opt == 'j' || opt == 'x') && flag) {
         return std::unexpected(ParseArgsErrors::multiple_compression_options);
-    } else {
-        return std::unexpected(ParseArgsErrors::unknown_option);
     }
-  }  
+  }
+  if (optind < argc) {
+    opciones.destino_backup = argv[optind];
+  }
+  if (optind == argc) {
+    if (argc == 1 || (argc == 2) && flag) {
+      opciones.destino_backup = get_current_dir();
+    }
+  }
   if (argc > 3) {
     return std::unexpected(ParseArgsErrors::too_many_arguments);
   }
@@ -403,28 +592,6 @@ std::expected<ServerOptions, ParseArgsErrors> parse_arguments(int argc, char* ar
 void write_usage() {
   std::string usage = "uso: backup-server [-z | -j | -x] [DIRECTORIO_DESTINO]\n";
   write(STDOUT_FILENO, usage.c_str(), usage.length());
-}
-
-std::string get_compression_command(ServerOptions::CompressionType compression) {
-  std::string compression_type;
-  switch (compression) {
-    case ServerOptions::CompressionType::BZIP2:
-      compression_type = "bzip2";
-      break;
-    
-    case ServerOptions::CompressionType::GZIP:
-      compression_type = "gzip";
-      break;
-
-    case ServerOptions::CompressionType::XZ:
-      compression_type = "xz";
-      break;
-
-    default:
-      compression_type = "none";
-      break;
-  }
-  return compression_type;
 }
 
 bool is_command_available(const std::string& command) {
@@ -444,8 +611,6 @@ bool is_command_available(const std::string& command) {
   }
   return false;
 }
-
-// POR EL PUNTO 1.5.4
 
 int main(int argc, char* argv[]) {
   auto args = parse_arguments(argc, argv);
@@ -470,26 +635,35 @@ int main(int argc, char* argv[]) {
     }
   }
   ServerOptions options = args.value();
+  std::string compresion = "backup-server: compresión: ";
   if (options.compression == ServerOptions::CompressionType::BZIP2) {
     if (!is_command_available("bzip2")) {
       std::string error = "backup-server: error: bzip2 no está instalado\n";
       write(STDERR_FILENO, error.c_str(), error.length());
       return EXIT_FAILURE;
+    } else {
+        compresion += "bzip2\n";
     }
   } else if (options.compression == ServerOptions::CompressionType::GZIP) {
       if (!is_command_available("gzip")) {
         std::string error = "backup-server: error: gzip no está instalado\n";
         write(STDERR_FILENO, error.c_str(), error.length());
         return EXIT_FAILURE;
+      } else {
+          compresion += "gzip\n";
       }
   } else if (options.compression == ServerOptions::CompressionType::XZ) {
       if (!is_command_available("xz")) {
         std::string error = "backup-server: error: xz no está instalado\n";
         write(STDERR_FILENO, error.c_str(), error.length());
         return EXIT_FAILURE;
+      } else {
+          compresion += "xz\n";
       }
   }
   
+  write(STDOUT_FILENO, compresion.c_str(), compresion.length());
+
   std::string var_env = get_work_dir();
   if (var_env.empty()) {
     const char* no_existe = "La variable de entorno BACKUP_WORK_DIR no está definida.\n";
@@ -499,13 +673,8 @@ int main(int argc, char* argv[]) {
   if (!directorio_correcto(var_env.c_str())) {
     return EXIT_FAILURE;
   }
-  const char* directorio{};
-  if (argc == 1) {
-    directorio = get_current_dir().c_str();
-  } else {
-      directorio = argv[1];
-  }
-  if (!directorio_correcto(directorio)) {
+
+  if (!directorio_correcto(options.destino_backup.c_str())) {
     return EXIT_FAILURE;
   }
   std::string ruta_pid = get_pid_file_path();
@@ -524,10 +693,7 @@ int main(int argc, char* argv[]) {
     buffer_pid.resize(tamaño);
     pid = buffer_pid;
     close(archivo_pid);
-  } else {
-      std::string error{"Error al abrir el archivo.\n"};
-      write(STDERR_FILENO, error.c_str(), error.length());
-  }
+  } 
   if (!pid.empty()) {
     pid_t pid_server = std::stoi(pid);
     if (is_server_running(pid_server)) {
@@ -564,6 +730,9 @@ int main(int argc, char* argv[]) {
     unlink(fifo.c_str());
     return EXIT_FAILURE;
   }
+  std::string esperando = "backup-server: esperando solicitudes de backup en ";
+  esperando += options.destino_backup + '\n';
+  write(STDOUT_FILENO, esperando.c_str(), esperando.length());
   int fifo_abierto = open(fifo.c_str(), O_RDONLY);
   if (fifo_abierto == -1) {
     if (errno == EINTR) {
@@ -581,6 +750,6 @@ int main(int argc, char* argv[]) {
     unlink(fifo.c_str());
     return EXIT_FAILURE;
   }
-  run_server(fifo_abierto, directorio);
+  run_server(fifo_abierto, options.destino_backup, options);
   return EXIT_SUCCESS;
 }
